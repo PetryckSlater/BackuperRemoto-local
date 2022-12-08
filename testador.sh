0a5c82136d840845ce5bf3446e035c7ff549b107
PetryckSlater <104164472+PetryckSlater@users.noreply.github.com>
2022-12-08 13:37:25 -0500

    Atualizado

--- testador.sh
+++ testador.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 #Uso do time e usr
-for i in  {1..10}; do
-	time ./index.sh
+function testertime {
+	for i in  {1..50}; do
+		timesgosto=$(time ./index.sh | grep time)
+		echo "$timesgosto" > infos.txt	
 done
+}
+
+function testememoria {
+	for i in {1..50}; do
+		usr  ./index.sh | grep usr
+	done
+}
+echo "Digite 1 para testar o timer ou 2 para o de memoria"
+read inf
+case $inf in
+	"1")
+		testertime
+		;;
+	"2")
+		testememoria
+		;;
+esac
+
